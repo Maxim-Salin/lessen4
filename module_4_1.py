f0d89bfa3b42fedbf44685e61810c5872b225942
Максим Салин <salin.maxim1973@gmail.com>
2024-12-08 18:39:16 +0300

    Домашняя работа по уроку "Модули и пакеты"
    Домашняя работа по уроку "Модули и пакеты"

---
+++ module_4_1.py
@@ -0,0 +1,7 @@
+import fake_math as f
+from true_math import *
+
+print(divide(89,7))
+print(f.divide(77,7))
+print(divide(89,0))
+print(f.divide(89,0))
---
+++ true_math.py
@@ -0,0 +1,7 @@
+#import math
+from math import inf
+def divide(first,second):
+        if second != 0:
+            return first/second
+        else:
+            return inf
